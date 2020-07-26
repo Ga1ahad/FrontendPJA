786b05a9583ee82db7c93d46f24a1d244f8a4a99
Adam Nguyen <adanguye@publicisgroupe.net>
2020-07-26 22:05:04 +0200

    Dodanie ekranu szafy

---
+++ src/screens/User/Wardrobe/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import './wardrobe.css';
+import Divider from '@material-ui/core/Divider';
+import Tooltip from '@material-ui/core/Tooltip';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+
+const rows = [
+    createData('https://source.unsplash.com/Wr0TpKqf26s', 'Czarny T-shirt', 'koszulki', 'koszulka, czarna, lato, lekka', '27.07.2020'),
+    createData('https://source.unsplash.com/I3C5ctmT8Z0', 'Niebieska koszula', 'koszule', 'koszule, niebieska, lato, formalna, elegancja', '27.07.2020'),
+    
+];
+
+function createData(address, name, type, tags, add_date) {
+    return { address, name, type, tags, add_date };
+  }
+
+const Wardrobe = () => {
+    return (
+        <div>
+            <Paper className="paper" > 
+                <h2>WARDROBE</h2>
+
+                    <Fab color="primary" className="tool">
+                        <AddIcon />
+                    </Fab>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="right"></TableCell>
+                                <TableCell align="right">Nazwa</TableCell>
+                                <TableCell align="right">Typ</TableCell>
+                                <TableCell align="right">Tagi</TableCell>
+                                <TableCell align="right">Data dodania</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row.address}>
+                                    <TableCell component="th" scope="row">
+                                        <img src={row.address} width="160" height="190"/>
+                                    </TableCell>
+                                    <TableCell align="right">{row.name}</TableCell>
+                                    <TableCell align="right">{row.type}</TableCell>
+                                    <TableCell align="right">{row.tags}</TableCell>
+                                    <TableCell align="right">{row.add_date}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </div>
+    )
+}
+
+export default Wardrobe;
