24c5bc83a0bce709d2609d2d36534fb9d09c1dc0
Adam Nguyen <adanguye@publicisgroupe.net>
2020-08-22 21:08:53 +0200

    Sidebar

--- src/screens/User/routes.js
+++ src/screens/User/routes.js
@@ -1,3 +1,58 @@
-export const loggedInRoutes = [
-  
-];
+import Wardrobe from "./Clothes/Wardrobe";
+import AddClothes from './Clothes/AddClothes.js';
+import AddTrip from './Trip/AddTrip.js';
+import ListTrip from './Trip/ListTrip.js';
+import { WorkRounded, CalendarTodayRounded, AddRounded, TodayRounded, CollectionsRounded } from '@material-ui/icons';
+
+const pathIds = {
+    wardrobe: "wardrobe",
+    todaysSet: "todays-set",
+    addCloth: "add-cloth",
+    listTrip: "list-trip",
+    addTrip: "add-trip",
+};
+
+const pathRouting = {
+    wardrobe: "/clothes/list",
+    todaySet: "/todaysset",
+    addCloth: "/clothes/add",
+    listTrip: "/trip/list",
+    addTrip: "/trip/add",
+};
+
+const loggedInRoutes = {
+    [pathIds.wardrobe]: {
+        path: pathRouting.wardrobe,
+        sidebarName: "SZAFA",
+        component: Wardrobe,
+        icon: CollectionsRounded,
+    },
+    [pathIds.todaySet]: {
+        path: pathRouting.todaySet,
+        sidebarName: "ZESTAW NA DZIŚ",
+        component: AddClothes,
+        icon: TodayRounded,
+    },
+    [pathIds.addCloth]: {
+        path: pathRouting.addCloth,
+        sidebarName: "DODAJ UBRNIE",
+        component: AddClothes,
+        icon: AddRounded,
+    },
+    [pathIds.listTrip]: {
+        path: pathRouting.listTrip,
+        sidebarName: "PODRÓŻE",
+        component: ListTrip,
+        icon: WorkRounded
+        ,
+
+    },
+    [pathIds.addTrip]: {
+        path: pathRouting.addTrip,
+        sidebarName: "PLANOWANIE PODRÓŻY",
+        component: AddTrip,
+        icon: CalendarTodayRounded,
+    },
+};
+
+export { loggedInRoutes, pathIds, pathRouting };
