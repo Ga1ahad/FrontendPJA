8e2efeb3fa8ed6889bc11a62d9928aeaa114c022
Adam Nguyen <adanguye@publicisgroupe.net>
2020-08-28 23:19:34 +0200

    Tabela gen

--- src/screens/User/Clothes/Wardrobe.js
+++ src/screens/User/Clothes/Wardrobe.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-
-import '../../index.css';
+import ReactTable from '../../ReactTable';
 
 const rows = [
   createData(
@@ -22,46 +18,22 @@ const rows = [
   ),
 ];
 
+const columns = [
+  { id: 'address', label: '' },
+  { id: 'name', label: 'Nazwa' },
+  { id: 'type', label: 'Typ' },
+  { id: 'tags', label: 'Tagi' },
+  { id: 'add_date', label: 'Data dodania' },
+];
+
 function createData(address, name, type, tags, add_date) {
   return { address, name, type, tags, add_date };
 }
+const siteName = 'WARDROBE'
 
 const Wardrobe = () => {
   return (
-    <div>
-      <Paper className="paper">
-        <h2>WARDROBE</h2>
-        <Fab color="primary" className="tool">
-          <AddIcon />
-        </Fab>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right"></TableCell>
-                <TableCell align="right">Nazwa</TableCell>
-                <TableCell align="right">Typ</TableCell>
-                <TableCell align="right">Tagi</TableCell>
-                <TableCell align="right">Data dodania</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.address}>
-                  <TableCell component="th" scope="row">
-                    <img src={row.address} width="160" height="190" alt="img" />
-                  </TableCell>
-                  <TableCell align="right">{row.name}</TableCell>
-                  <TableCell align="right">{row.type}</TableCell>
-                  <TableCell align="right">{row.tags}</TableCell>
-                  <TableCell align="right">{row.add_date}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </div>
+    <ReactTable siteName={siteName} columns={columns} rows={rows} />
   );
 };
 
--- src/screens/User/Trip/ListTrip.js
+++ src/screens/User/Trip/ListTrip.js
@@ -1,54 +1,27 @@
 import React from 'react'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Fab } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
+import ReactTable from '../../ReactTable';
 
-import '../../index.css';
 
 const rows = [
     createData('Pierwsza podróż', '20.7.2020', '26.7.2020', 'Kraków, Polska', '12.7.2020 12:20'),
     createData('Druga podróż', '18.9.2020', '26.9.2020', 'Dublin, Irlandia', '8.9.2020 14:26'),
 
 ];
-
-function createData(name, start, end, city, country, add_date) {
-    return { name, start, end, city, country, add_date };
+const columns = [
+    { id: 'name', label: 'Nazwa' },
+    { id: 'start', label: 'Początek' },
+    { id: 'end', label: 'Koniec' },
+    { id: 'city_country', label: 'Miejsce' },
+    { id: 'add_date', label: 'Data dodania' },
+];
+function createData(name, start, end, city_country, add_date) {
+    return { name, start, end, city_country, add_date };
 }
+const siteName = 'PODRÓŻE'
 
 const ListTrip = () => {
     return (
-        <div>
-            <Paper className="paper" >
-                <h2>PODRÓŻE</h2>
-                <Fab color="primary" className="tool">
-                    <AddIcon />
-                </Fab>
-                <TableContainer component={Paper}>
-                    <Table aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align="right">Nazwa</TableCell>
-                                <TableCell align="right">Początek</TableCell>
-                                <TableCell align="right">Koniec</TableCell>
-                                <TableCell align="right">Miejsce</TableCell>
-                                <TableCell align="right">Data dodania</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow>
-                                    <TableCell align="right">{row.name}</TableCell>
-                                    <TableCell align="right">{row.start}</TableCell>
-                                    <TableCell align="right">{row.end}</TableCell>
-                                    <TableCell align="right">{row.city}</TableCell>
-                                    <TableCell align="right">{row.country}</TableCell>
-                                    <TableCell align="right">{row.add_date}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Paper>
-        </div>
+        <ReactTable siteName={siteName} columns={columns} rows={rows} />
     )
 }
 
