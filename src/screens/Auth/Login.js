3df91b5be6f62529bd7cd65bc1fd4bd87ec2cb7c
Adam Nguyen <adanguye@publicisgroupe.net>
2020-11-08 20:10:12 +0100

    Login

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -12,7 +12,7 @@ const LoginSchema = Yup.object().shape({
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(14),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
