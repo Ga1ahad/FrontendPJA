4898e6c9b8f348c31f12583c8b7c6b13e4eb8d2a
s15236 <s15236@pjwstk.edu.pl>
2020-11-25 22:14:14 +0100

    fix of imports

--- src/screens/ReactTable.js
+++ src/screens/ReactTable.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Fab } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
 import './index.css';
 
 const useStyles = makeStyles({
